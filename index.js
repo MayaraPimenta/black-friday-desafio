38545f33006b49e0a9e704e136b15b67f506cbe5
Mayara Pimenta <mayaralrpimenta@gmail.com>
2021-07-14 19:17:24 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const router = express.Router();
+
+// app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'pages/index.html'));
+})
+
+app.use('/', router);
+
+app.listen(3000);
